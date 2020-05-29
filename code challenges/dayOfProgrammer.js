a9c1873e24f665961c7156863d3f22e2dda29d37
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-05-29 13:59:33 +0100

    add dayOfProgrammer Challenge solutions

--- code challenges/dayOfProgrammer.js
+++ code challenges/dayOfProgrammer.js
@@ -38,3 +38,14 @@ function dayOfProgrammer(year) {
   result = day(leapCheck, year);
   console.log(result);
 }
+
+// Shortened code
+
+function dayOfProgrammer(year) {
+  return (year === 1918) ? `26.09.${year}` : 
+  ((year < 1918 && year % 4 === 0) || 
+  (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0))) ?
+  `12.09.${year}` : `13.09.${year}`;
+
+
+}
